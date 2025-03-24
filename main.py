11668d05120ba26b404069195133bbd8a12ed0a6
mA7-r1x <mA7r1xOfficial@proton.me>
2025-03-24 11:41:13 -0400

    Player Fully Movable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
@@ -7,6 +8,8 @@ clock = pygame.time.Clock()
 dt = 0
 dt = clock.tick(60) / 1000
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
     print("Starting Asteroids!")
@@ -16,7 +19,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     while True:
@@ -24,7 +26,10 @@ def main():
             if event.type == pygame.QUIT:  # If the user clicks the close button
                 return  # Exit the game loop and thus the `main()` function
         
+        player.update(dt)
+
         screen.fill((0, 0, 0))  # Fill screen with black
+        player.draw(screen)
         pygame.display.flip()   # Refresh the display
 
 # Don't forget to call the main function to start your program!
